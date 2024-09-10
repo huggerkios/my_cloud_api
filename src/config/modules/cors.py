a7ee02fe3f2dcfe3459429a66e0fddc414f704c9
huggerkios <hugger.kiosk0r@icloud.com>
2024-09-10 21:16:28 +0300

    update cors.py

--- src/config/modules/cors.py
+++ src/config/modules/cors.py
@@ -1,10 +1,2 @@
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:3030",
-    "http://localhost:5173",
-    "http://localhost",
-    # Дополнительные разрешенные источники, если есть
-]
-
-CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
+CORS_ALLOW_ALL_ORIGINS = True
