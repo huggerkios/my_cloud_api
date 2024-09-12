f3e3511efd4f1cd768ad65cca92fa456935de89a
huggerkios <hugger.kiosk0r@icloud.com>
2024-09-12 17:29:31 +0300

    Update urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -21,3 +23,6 @@ urlpatterns = [
     path("api/v1/cloud/", include("apps.cloud.urls")),
     path("api/v1/", include("apps.administration.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
